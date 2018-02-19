439b22c6c8b01d479aca35d085b5fd3b7973a344
Sergey Kojin <sergey.kojin@gmail.com>
2018-02-19 12:28:49 +0300

    restore tests

--- test.rb
+++ test.rb
@@ -24,98 +24,98 @@ end
 
 # TESTS ARE BELOW
 
-# # test simple command
-# test_command('watch' => 'echo 111',
-#   'do' => 'echo $1 > unit.do') do |name, conf|
-#   # on first call call :do
-#   Command.new(name, conf).call
-#   assert_equal "111\n", command_result
+# test simple command
+test_command('watch' => 'echo 111',
+  'do' => 'echo $1 > unit.do') do |name, conf|
+  # on first call call :do
+  Command.new(name, conf).call
+  assert_equal "111\n", command_result
 
-#   # result is same, :do not called
-#   Command.new(name, conf).call
-#   assert_nil command_result
+  # result is same, :do not called
+  Command.new(name, conf).call
+  assert_nil command_result
 
-#   # result is changed, :do called
-#   conf['watch'] = 'echo 222'
-#   Command.new(name, conf).call
-#   assert_equal "222\n", command_result
-# end
+  # result is changed, :do called
+  conf['watch'] = 'echo 222'
+  Command.new(name, conf).call
+  assert_equal "222\n", command_result
+end
 
-# # test command $1 and $2
-# test_command('watch' => 'echo 111',
-#   'do' => 'echo "$1-$2" > unit.do') do |name, conf|
-#   Command.new(name, conf).call
-#   conf['watch'] = 'echo 222'
-#   Command.new(name, conf).call
-#   assert_equal "222-111\n", command_result
-# end
+# test command $1 and $2
+test_command('watch' => 'echo 111',
+  'do' => 'echo "$1-$2" > unit.do') do |name, conf|
+  Command.new(name, conf).call
+  conf['watch'] = 'echo 222'
+  Command.new(name, conf).call
+  assert_equal "222-111\n", command_result
+end
 
-# # test :enabled
-# test_command('watch' => 'echo 111',
-#   'enabled' => false,
-#   'do' => 'echo $1 > unit.do') do |name, conf|
-#     Command.new(name, conf).call
-#     assert_nil command_result
-#     conf['enabled'] = true
-#     Command.new(name, conf).call
-#     assert_equal "111\n", command_result
-# end
+# test :enabled
+test_command('watch' => 'echo 111',
+  'enabled' => false,
+  'do' => 'echo $1 > unit.do') do |name, conf|
+    Command.new(name, conf).call
+    assert_nil command_result
+    conf['enabled'] = true
+    Command.new(name, conf).call
+    assert_equal "111\n", command_result
+end
 
-# # test :skip_error
-# test_command('watch' => 'echo 111 | grep 222', # grep if not found exit with 256 code
-#   'skip_error' => true, # with this option do not stop command processing, useful if curl sometimes return bad code
-#   'do' => 'echo $1 > unit.do') do |name, conf|
-#     Command.new(name, conf).call
-#     assert_nil command_result
+# test :skip_error
+test_command('watch' => 'echo 111 | grep 222', # grep if not found exit with 256 code
+  'skip_error' => true, # with this option do not stop command processing, useful if curl sometimes return bad code
+  'do' => 'echo $1 > unit.do') do |name, conf|
+    Command.new(name, conf).call
+    assert_nil command_result
 
-#     conf['skip_error'] = false
-#     Command.new(name, conf).call
-#     assert_equal "\n", command_result
-# end
+    conf['skip_error'] = false
+    Command.new(name, conf).call
+    assert_equal "\n", command_result
+end
 
-# # less then
-# test_command('watch' => 'echo 10',
-#   'lt' => '5',
-#   'do' => 'echo "$1 - $lt" > unit.do') do |name, conf|
-#     Command.new(name, conf).call
-#     assert_equal "10 - 5\n", command_result
-#     conf['watch'] = 'echo 5'
-#     Command.new(name, conf).call
-#     assert_nil command_result
+# less then
+test_command('watch' => 'echo 10',
+  'lt' => '5',
+  'do' => 'echo "$1 - $lt" > unit.do') do |name, conf|
+    Command.new(name, conf).call
+    assert_equal "10 - 5\n", command_result
+    conf['watch'] = 'echo 5'
+    Command.new(name, conf).call
+    assert_nil command_result
 
-#     conf['watch'] = 'echo 4'
-#     Command.new(name, conf).call
-#     assert_equal "4 - 5\n", command_result
-# end
+    conf['watch'] = 'echo 4'
+    Command.new(name, conf).call
+    assert_equal "4 - 5\n", command_result
+end
 
-# # great then
-# test_command('watch' => 'echo 1',
-#   'gt' => '5',
-#   'do' => 'echo "$1 - $gt" > unit.do') do |name, conf|
-#     Command.new(name, conf).call
-#     assert_equal "1 - 5\n", command_result
-#     conf['watch'] = 'echo 5'
-#     Command.new(name, conf).call
-#     assert_nil command_result
+# great then
+test_command('watch' => 'echo 1',
+  'gt' => '5',
+  'do' => 'echo "$1 - $gt" > unit.do') do |name, conf|
+    Command.new(name, conf).call
+    assert_equal "1 - 5\n", command_result
+    conf['watch'] = 'echo 5'
+    Command.new(name, conf).call
+    assert_nil command_result
 
-#     conf['watch'] = 'echo 6'
-#     Command.new(name, conf).call
-#     assert_equal "6 - 5\n", command_result
-# end
+    conf['watch'] = 'echo 6'
+    Command.new(name, conf).call
+    assert_equal "6 - 5\n", command_result
+end
 
-# # equal - :do when equal or not equal
-# test_command('watch' => 'echo 5',
-#   'eq' => '5',
-#   'do' => 'echo "$1 - $eq" > unit.do') do |name, conf|
-#     Command.new(name, conf).call
-#     assert_equal "5 - 5\n", command_result
-#     Command.new(name, conf).call
-#     assert_nil command_result
+# equal - :do when equal or not equal
+test_command('watch' => 'echo 5',
+  'eq' => '5',
+  'do' => 'echo "$1 - $eq" > unit.do') do |name, conf|
+    Command.new(name, conf).call
+    assert_equal "5 - 5\n", command_result
+    Command.new(name, conf).call
+    assert_nil command_result
 
-#     conf['watch'] = 'echo 6'
-#     Command.new(name, conf).call
-#     assert_equal "6 - 5\n", command_result
-# end
+    conf['watch'] = 'echo 6'
+    Command.new(name, conf).call
+    assert_equal "6 - 5\n", command_result
+end
 
 def test_parse_every_conf
   assert_equal 2 * 60, Command.new('test', 'every' => '2m').every
